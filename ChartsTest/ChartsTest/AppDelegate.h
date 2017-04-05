1c409a5f3988caeab69582c74449b7f3b5576643
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-04-05 22:13:58 +0300

    Initial commit

---
+++ ChartsTest/ChartsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartsTest
+//
+//  Created by Stanly Shiyanovskiy on 05.04.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
